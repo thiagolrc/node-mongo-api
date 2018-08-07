d14c1aaf0045fb355067ac9706fd06fc2c0e21d8
Thiago Lourenconi <thiago.lourenconi@olxbr.com>
2018-08-07 16:44:00 -0300

    Adds a Document to a collection, logging results and getting timestamp from ID

--- playground/mongo-db-connect.js
+++ playground/mongo-db-connect.js
@@ -15,6 +15,18 @@ MongoClient.connect(url, (err, client) => {
     console.log("Connected successfully to server");
 
     const db = client.db(dbName);
-    
+
+    var toDo = {
+        text: 'Something to do',
+        completed: true
+    };
+    db.collection('Todos').insertOne(toDo, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert ToDo', err);
+        }
+        console.log('Todo inserted into mongo', toDo);
+        console.log(JSON.stringify(result, undefined, 2));
+        console.log(result.ops[0]._id.getTimestamp()); //how to get the timestamp from ID
+    });
     client.close();
 });
