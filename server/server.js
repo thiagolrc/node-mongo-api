b0fdaf2b1f3e524ebf0ddb53ced08e606b77a882
Thiago Lourenconi <thiago.lourenconi@olxbr.com>
2018-08-08 16:39:07 -0300

    Sets up an web app usingexpress framework and body-parser

--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+var express = require('express');
+var body_parser = require('body-parser');
+
+var app = express();
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/Todo');
 //same as var mongoose = require('./db/mongoose').mongoose
@@ -10,4 +15,9 @@ newTodo.save().then(doc=>{
     console.log('Saved', doc);
 }, err => {
     console.log('Could not save Todo', err);
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running and listening on port ${port}`);
 });
