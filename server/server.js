e962b7dc06068384041e6fdc14ed9f365659935a
Thiago Lourenconi <thiago.lourenconi@olxbr.com>
2018-08-13 11:53:17 -0300

    Converting GET '/todos/:id' to async await version

--- server/server.js
+++ server/server.js
@@ -34,19 +34,22 @@ app.get('/todos', (req, res) => {
     );
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', async (req, res) => {
     var id = req.params.id;
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
-    Todo.findById(id).then(
-        todo => {
-            if (!todo) {
-                return res.status(404).send();
-            } 
-            res.send(todo);
-        }
-    ).catch(err => res.status(404).send(err));
+
+    try{
+        var todo = await Todo.findById(id);
+
+        if (!todo) {
+            return res.status(404).send();
+        } 
+        res.send(todo);
+    }catch(e) {
+        res.status(404).send(e)
+    }
 });
 
 var port = process.env.PORT || 3000;
